c847896b05ad83fe92e940f6f01813802348e146
listanegra <vitormac3@hotmail.com>
2021-07-07 22:14:42 -0300

    Accidents service implementation

--- routes/api.ts
+++ routes/api.ts
@@ -1,5 +1,20 @@
 import { Router } from 'express'
+import { get, toNumber } from 'lodash'
+
+import Accidents from '../services/accidents-service'
 
 const router = Router();
+
+router.get('/accidents/city', (req, res) => {
+    const name = get(req.query, 'name') as string;
+
+    const offset = toNumber(get(req.query, 'offset', 0));
+    const limit = toNumber(get(req.query, 'limit', 50));
+
+    Accidents.getByCity(name, offset, limit)
+        .then(accidents => {
+            res.status(200).json(accidents);
+        });
+});
 
 export { router };
---
+++ services/accidents-service.ts
@@ -0,0 +1,26 @@
+import { upperCase } from 'lodash'
+
+import Mongo from '../database/connection'
+import Accident from '../models/accident'
+
+class Accidents {
+
+    public static getByCity(name: string, offset: number = 0, limit: number = 50): Promise<Geolocation[]> {
+        const city = upperCase(name);
+
+        return Mongo.db
+            .collection<Accident>('accidents')
+            .find({ municipio: city })
+            .project<Geolocation>({
+                _id: false,
+                lat: '$latitude',
+                lng: '$longitude'
+            })
+            .skip(offset)
+            .limit(limit)
+            .toArray();
+    }
+
+}
+
+export default Accidents;
